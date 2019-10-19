fceeea6bfb96e3d041d5d85b648737e8dbc58441
Samantha Garland <sjg41@duke.edu>
2019-10-19 13:11:23 -0400

    starting to work on manual code

--- manual.py
+++ manual.py
@@ -1,1 +1,23 @@
 # human solution
+# begin by combing through grid and seeing if there are any actual possibilities
+
+def check_row(puzzle, box):
+    pass
+
+def check_col(puzzle, box):
+    pass
+
+def check_box(puzzle, box):
+    pass
+
+def manually_solve(puzzle):
+    n = len(puzzle)
+    for i in range(n):
+        for j in range(n):
+            box = [i,j]
+            if puzzle[i][j] != 0:
+                pass
+                # violations = check_row(puzzle, box) 
+
+if __name__ == '__main__':
+    pass
