43dfeab6bff668232783ef9033a5723cb21f63a7
mallory-hahn <mallory.hahn@duke.edu>
2019-10-31 15:50:51 -0400

    exact cover matrix done

--- algx.py
+++ algx.py
@@ -21,30 +21,104 @@ board_size = 9
 # total of 324 contsratings (81*4) == columns
 # 729 rows in matrix (9^3)
 
-def base_sudoku_grid(){
+def base_sudoku_grid():
     matrix = []
     # set matrix size to be [size^3][size^2 * 4]
     # size^3 to represent all of the boxes, columns, and rows
     # size^2 * 4 to represent size of board and the four constraints
 
     # initialize matrix
-    for row in range(board_size**3):
+    # 324 columns
+    for col in range(board_size**3):
         temp_array = []
-        for col in range((board_size**2)*4)):
-            temp_array.append(-1)
+        # 729 rows
+        for row in range((board_size**2)*4):
+            # set everything to 0 at first
+            temp_array.append(0)
         matrix.append(temp_array)
+    # matrix:
+    #       row constraint ... col constraint ... box constraint ... cell constraint
+    #       123456789 123456789 x 81 x 4
+    # r1c1
+    # r1c1
+    # r1c1
+    #...
+    # r1c2
+    # r1c2
+    # ...
+    # r1c3
+    # ...
 
-    base = 0
-    # go through row-column constraints
-    for
+    # row constraints ... #col<81
+    col_delta = 0
+    row_delta = 0
+    num_rows = 0
+    for row in matrix:
+        #for row_constraint in range(81):
+        # fill in all of the possible numbers for r1, r2, r3, etc.
+        row[col_delta + row_delta] = 1
+        row_delta += 1
+        if row_delta == 9:
+            row_delta = 0
+            num_rows += 1
+        if num_rows == 9:
+            num_rows = 0
+            col_delta += 9
+        if col_delta == 81:
+            break
 
-}
+    # col constraints ... #col>=81 && #col<162
+    col_delta = 81
+    row_delta = 0
+    for row in matrix:
+        row[col_delta + row_delta] = 1
+        row_delta += 1
+        if row_delta == 81:
+            row_delta = 0
 
-def get_matrix(ny_times_puzzle, universe):
-    matrix = []
+    # block constraint
+    col_delta = 162
+    row_delta = 0
+    num_rows = 0
+    block_delta = 0
+    next_block = 0
+    for row in matrix:
+        row[col_delta + row_delta] = 1
+        row_delta += 1
+        # move onto next row
+        if row_delta == 9:
+            row_delta = 0
+            num_rows += 1
+        if num_rows == 3:
+            num_rows = 0
+            col_delta += 9
+            block_delta += 1
+        if block_delta == 3:
+            block_delta = 0
+            col_delta -= 27
+            row_delta = 0
+            num_rows = 0
+            next_block += 1
+        if next_block == 3:
+            col_delta += 27
+            next_block = 0
+        if col_delta == 243:
+            break
 
+    # cell constraint
+    row_delta = 0
+    col_delta = 243
+    for row in matrix:
+        print row_delta
+        row[col_delta + row_delta] = 1
+        row_delta += 1
+        if col_delta == 324:
+            break
 
     return matrix
+
+
+
 
 if __name__ == '__main__':
     ny_times_correct = np.array([[2,3,4,9,5,6,7,8,1],
@@ -66,6 +140,10 @@ if __name__ == '__main__':
                          [6,7,0,0,0,0,2,9,0],
                          [0,0,0,4,0,0,6,1,0]])
     universe = [1,2,3,4,5,6,7,8,9]
-    ny_times_matrix = get_matrix(ny_times_puzzle, universe)
-    our_solution = solve(ny_times_puzzle)
-    print(np.array_equal(our_solution,ny_times_correct))
+    base_matrix = base_sudoku_grid()
+    print len(base_matrix)
+    print len(base_matrix[0])
+    print base_matrix[486][216]
+    # ny_times_matrix = get_matrix(ny_times_puzzle, universe)
+    # our_solution = solve(ny_times_puzzle)
+    # print(np.array_equal(our_solution,ny_times_correct))
