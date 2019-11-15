bab19b48646fc34588a78d380ddbe8417e2822ec
oettinger.justinSurface <oettinger.justin@gmail.com>
2019-11-15 14:52:19 -0500

    fixed columns, working on blocks, done by end of night

--- dancingLinks.py
+++ dancingLinks.py
@@ -1,32 +1,6 @@
 import numpy as np
 
 def buildMat(board_size, puzzle):
-#    #vertical matDim is dimension ^ 3
-#    rows = dimension ** 3
-#    #horizontal matDim is (dimension ** 2) * 4
-#    cols = (dimension ** 2) * 4
-#    ourMat = np.zeros((rows, cols))
-#
-#    #build it based on general version first
-#
-#    for i in range(cols / 4):
-#        for j in range(rows):
-#            counter = j // dimension    #using floor division just to ensure behavior
-#            if counter == i:
-#                ourMat[j][i] = 1
-#            if j % dimension == i % dimension:
-#                #there needs to be one more condition here
-#                if j // 9 == i:
-#                    ourMat[j][i + cols/4] = 1
-#                    ourMat[j][i + 2 * (cols/4)] = 1
-#                    ourMat[j][i + 3 * (cols/4)] = 1
-#    #built general matrix, now i need to go back through
-#    #and change values to reflect our specific puzzle
-#    return ourMat
-
-#for now, using mallory's code since it works to set up the full standard matrix
-#may return to this later, but for now we have min. viable so i'll move forward
-
     matrix = []
     # set matrix size to be [size^3][size^2 * 4]
     # size^3 to represent all of the boxes, columns, and rows
@@ -63,26 +37,26 @@ def buildMat(board_size, puzzle):
         # fill in all of the possible numbers for r1, r2, r3, etc.
         row[col_delta + row_delta] = 1
         row_delta += 1
-        if row_delta == 9:
+        if row_delta == board_size:
             row_delta = 0
             num_rows += 1
-        if num_rows == 9:
+        if num_rows == board_size:
             num_rows = 0
-            col_delta += 9
-        if col_delta == 81:
+            col_delta += board_size
+        if col_delta == board_size**2:
             break
 
     # col constraints ... #col>=81 && #col<162
-    col_delta = 81
+    col_delta = board_size**2
     row_delta = 0
     for row in matrix:
         row[col_delta + row_delta] = 1
         row_delta += 1
-        if row_delta == 81:
+        if row_delta == board_size**2:
             row_delta = 0
 
     # block constraint
-    col_delta = 162
+    col_delta = (board_size**2) * 2
     row_delta = 0
     num_rows = 0
     block_delta = 0
@@ -91,82 +65,132 @@ def buildMat(board_size, puzzle):
         row[col_delta + row_delta] = 1
         row_delta += 1
         # move onto next row
-        if row_delta == 9:
+        if row_delta == board_size:
             row_delta = 0
             num_rows += 1
-        if num_rows == 3:
+        if num_rows == int(np.sqrt(board_size)):
             num_rows = 0
-            col_delta += 9
+            col_delta += board_size
             block_delta += 1
-        if block_delta == 3:
+        if block_delta == int(np.sqrt(board_size)):
             block_delta = 0
-            col_delta -= 27
+            col_delta -= int(np.sqrt(board_size)**3)
             row_delta = 0
             num_rows = 0
             next_block += 1
-        if next_block == 3:
-            col_delta += 27
+        if next_block == int(np.sqrt(board_size)):
+            col_delta += int(np.sqrt(board_size)**3)
             next_block = 0
-        if col_delta == 243:
-            break
 
     # cell constraint
     row_delta = 0
-    col_delta = 243
+    # col_delta = (np.sqrt(board_size)**3) * board_size
+    col_delta = (board_size**2) * 3
+    next_row = 0
     for row in matrix:
-        row[col_delta + row_delta] = 1
-        row_delta += 1
-        if col_delta == 324:
-            break
-        if row_delta == 81:
-            break
+        row[col_delta] = 1
+        next_row += 1
+        if next_row == board_size:
+            col_delta += 1
+            next_row = 0
 
-    #at this point, the base matrix is built, now to edit it for the puzzle specific one
     return matrix
 
 def puzzleSpecific(matrix, puzzle):
-    dimension = len(puzzle[0])
-    for rowIndex in range(len(puzzle)):
-        row = puzzle[rowIndex]
-        for column in range(len(row)):
-            if row[column] != 0:
-                thisNum = row[column] - 1
+    board_size = len(puzzle)
+    already_changed = []
+    # 9 elems per rxcx
+    matrix_row_index = 0
+    matrix_col_index = 0
+    starting_row = 0
+    col_starting_column = (board_size ** 2)
+    block_starting_column = (board_size ** 2) * 2
+    cell_starting_column = (board_size**2) * 3
+    for row in puzzle:
+        elems_in_row = set(row)
+        # part of row constraint
+        for elem in row:
+            # no element, but mark the other elems in the row as  0
+            if elem == 0:
+                row_index = starting_row
+                col_index = matrix_col_index
+                for sub_elem in elems_in_row:
+                    if sub_elem != 0:
+                        matrix[row_index+sub_elem-1][col_index+sub_elem-1] = 0
+            # there is already an element in this space
+            if elem != 0:
+                # index for elem to keep 1 is elem - 1
+                index_elem = elem - 1
 
-                #row column changes
-                rcSpacer = dimension * rowIndex
-                for i in range(dimension):
-                    matrix[rcSpacer + i][column + 3 * (dimension ** 2)] = 0
-                    if i == thisNum:
-                        matrix[rcSpacer + i][column + 3 * (dimension ** 2)] = 1
-                for i in range(dimension):
-                    if i != thisNum:
-                        matrix[(rcSpacer + i) + (dimension * i)][(i * column) + 3 * (dimension ** 2)] = 0
-                
-                #row contradiction changes
-                for i in range(dimension):
-                    matrix[(dimension * (i + (dimension * rowIndex))) + thisNum][column + thisNum] = 0
-                    if i == rowIndex:
-                        for j in range(dimension):
-                            matrix[(dimension * (i + (dimension * rowIndex))) + j][column + j] = 0
-                        matrix[(dimension * (i + (dimension * rowIndex))) + thisNum][column + thisNum] = 1
+                # adjust row constraints
+                for index in range(board_size):
+                    x = matrix_row_index + index
+                    y = matrix_col_index + index
+                    if index != index_elem:
+                        if (x,y) not in already_changed:
+                            # temp_matrix_col_index = matrix_col_index
+                            matrix[x][y] = 0
+                    else:
+                        matrix[x][y] = 1
+                        already_changed.append((x,y))
 
-                #column contradiction changes
-                for i in range(dimension):
-                    matrix[thisNum + ((dimension ** 2) * i)][thisNum + (column * dimension) + 2 * (dimension ** 2)] = 0
-                    if i == rowIndex:
-                        for j in range(dimension):
-                            matrix[j + ((dimension ** 2) * i)][i + (column * dimension) + 2 * (dimension ** 2)] = 0
-                        matrix[thisNum + ((dimension ** 2) * i)][thisNum + (column * dimension) + 2 * (dimension ** 2)] = 1
+                # adjust col constraints
+                for index in range(board_size):
+                    x = matrix_row_index + index
+                    i = list(row).index(elem)
+                    j = matrix_col_index * list(row).index(elem)
+                    y = col_starting_column + index + (board_size * list(row).index(elem))
+                    if index != index_elem:
+                        if (x,y) not in already_changed:
+                            matrix[x][y] = 0
+                    else:
+                        matrix[x][y] = 1
+                        already_changed.append((x,y))
+                for i in range(board_size ** 3):
+                    print(matrix[i][col_starting_column:col_starting_column + board_size**2])
+                print("hi")
+                # adjust block constraints
+                for index in range(board_size):
+                    x = matrix_row_index + index
+                    y = block_starting_column + index
+                    if index != index_elem:
+                        if (x,y) not in already_changed:
+                            matrix[x][y] = 0
+                    else:
+                        matrix[x][y] = 1
+                        already_changed.append((x,y))
 
-                #box contradiction changes
+                # adjust cell constraints
+                for index in range(board_size):
+                    # only need to check if cell has something or not
+                    # if cell has an element, then set the rest of the elements
+                    # in that cel lblock to 0
+                    x = matrix_row_index + index
+                    y = cell_starting_column
+                    if index != index_elem:
+                        if (x,y) not in already_changed:
+                            matrix[x][y] = 0
+                    else:
+                        already_changed.append((x,y))
+
+
+            matrix_row_index += board_size
+            starting_row += board_size
+            cell_starting_column += 1
+            matrix_col_index += board_size
+        # if matrix_row_index == 324 or matrix_col_index == 324:
+        #     break
+        # matrix_row_index += 81
+        # starting_row += board_size
+    return matrix
           
 
 
 def dancingLinks(puzzle, ourMat):
     dimension = len(puzzle[0])
     
-    rows = dimension ** 3   #need to change this to ourMat's current dimensions
-    cols = (dimension ** 2) * 4
+    rows = len(ourMat[0])  #need to change this to ourMat's current dimensions
+    cols = len(ourMat)
     
     minOnes = float("inf")
     colLeastOnes = -1
@@ -214,6 +238,12 @@ if __name__ == '__main__':
                          [6,7,0,0,0,0,2,9,0],
                          [0,0,0,4,0,0,6,1,0]])
     universe = [1,2,3,4,5,6,7,8,9]
+    
+    puzzle = np.array([[1,0,3,0],
+                      [0,0,0,2],
+                      [4,0,0,0],
+                      [0,1,0,3]])
+    dimension = 4
 
     ourMat = buildMat(dimension, puzzle)
     ourMat = puzzleSpecific(ourMat, puzzle)
