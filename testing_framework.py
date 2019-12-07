d24bb6237105fdf942dc6baa12fa2892dfe4ca55
oettinger.justinSurface <oettinger.justin@gmail.com>
2019-12-06 19:48:39 -0500

    my final changes to the testing framework to have included dancing links

--- testing_framework.py
+++ testing_framework.py
@@ -37,42 +37,42 @@ def dancing_time(puzzle):
 
 if __name__ == '__main__':
     # ## Test 4x4 Puzzles ##
-    puzzles = get_4_puzzles()
-    back = 0
-    brute = 0
-    manual = 0
-    dancing = 0
+    #puzzles = get_4_puzzles()
+    #back = 0
+    #brute = 0
+    #manual = 0
+    #dancing = 0
 
-    for p in puzzles:
+    #for p in puzzles:
         #brute += brute_time(p)
         #back += back_time(p)
         #manual += manual_time(p)
-        dancing += dancing_time(p)
+        #dancing += dancing_time(p)
 
-    print("Testing 4x4 Puzzle Efficiencies for 10 puzzles:")
+    #print("Testing 4x4 Puzzle Efficiencies for 10 puzzles:")
     # print("Brute Force: {}".format(brute))
     # print("Backtracking: {}".format(back))
     # print("Manual: {}".format(manual))
-    print("Dancing: {}".format(dancing))
+    #print("Dancing: {}".format(dancing))
 
     ## Test 9x9 Puzzles ##
-    #quizzes, solutions = get_puzzles()
-    #back = 0
-    #manual = 0
-    #dancing = 0
-    #trial = 0
+    quizzes, solutions = get_puzzles()
+    back = 0
+    manual = 0
+    dancing = 0
+    trial = 0
     
-    #sample = random.sample(range(1000000),1000)
-        
-    #for s in sample:
-    #    print("Trial number: " + str(trial))
-    #    puz = quizzes[s]
-    #    #back += back_time(puz)
-    #    # manual += manual_time(puz)
-    #    dancing += dancing_time(puz)
-    #    trial += 1
+    sample = random.sample(range(1000000),1000)
+    print("Starting trials")   
+    for s in sample:
+        #print("Trial number: " + str(trial))
+        puz = quizzes[s]
+        #back += back_time(puz)
+        # manual += manual_time(puz)
+        dancing += dancing_time(puz)
+        #trial += 1
     
     #print("Testing 9x9 Puzzle Efficiencies for 1000 puzzles:")
     #print ("Backtracking: {}".format(back))
     #print("Manual: {}".format(manual))
-    #print("Dancing: {}".format(dancing))
+    print("Dancing: {}".format(dancing))
